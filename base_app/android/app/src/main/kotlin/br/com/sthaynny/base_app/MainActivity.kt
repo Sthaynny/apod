dd6fc0f6d77923c84f3aab286828d042f4f9a576
sthaynny <igorsthaynny@gmail.com>
2021-11-11 22:11:10 -0300

    feat: criando micro front ends

---
+++ base_app/android/app/src/main/kotlin/br/com/sthaynny/base_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.sthaynny.base_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ micro_commoms_dependency/lib/micro_commoms_dependency.dart
@@ -0,0 +1,7 @@
+library micro_commoms_dependency;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
